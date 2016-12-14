97089a96b0c2ab75d031fd4614c9d0a27729c409
Jesse Lamb <jesselamb@builddirect.com>
2016-12-13 17:20:45 -0800

    existing working, uploading almost done

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const { newClient, existingContent, upload } = require('./src/s3');
+const { newClient, getExistingFiles: getExistingS3Files, uploadFile: s3Upload } = require('./src/s3');
+const fs = require('fs');
 const minimist = require('minimist');
+const path = require('path');
 const util = require('util');
 
 const argMap = minimist(process.argv.slice(2)); // expects arguments in --key=value format
 const { USE_ROLE } = process.env;
 
 const { accessKeyId, secretAccessKey, bucket, src, dest, region } = argMap;
+const EXISTING_FILE_ERROR_MSG = 'Failed to upload! Destination path contains existing content';
+
+function errorMessage(message) {
+  console.error(`${message}\n`);
+  process.exit(1);
+}
 
 const requiredArgs = [
   'bucket',
@@ -15,18 +23,42 @@ const requiredArgs = [
 ].filter(a => argMap[a] === undefined);
 
 if (requiredArgs.length > 0) {
-  console.error(`Missing argument/s: ${requiredArgs.join(', ')}`, '\n');
-  process.exit(1);
+  errorMessage(`Missing argument/s: ${requiredArgs.join(', ')}`, '\n');
 }
 
 if (!USE_ROLE && (!accessKeyId || !secretAccessKey)) {
-  console.error('Missing credentials\n');
-  process.exit(1);
+  errorMessage('Missing credentials');
 }
 
-existingContent(bucket, newClient({ accessKeyId, secretAccessKey })).then(contents => {
-  console.log(util.inspect(contents, { showHidden: true, depth: null, colors: true }));
-  
-}).catch(error => {
-  console.log({ error });
+const intersection = (...arrays) =>
+  [...new Set([].concat(...arrays))].filter(toFind =>
+    arrays.every(arr => arr.some(el => el === toFind)
+  )
+);
+
+const getExistingFiles = directory => fs.readdirSync(path.resolve(directory));
+
+const s3Client = newClient({ accessKeyId, secretAccessKey });
+
+function existing(files) {
+  const existingFiles = getExistingFiles(src);
+  const intersect = intersection(existingFiles, files);
+  if (intersect.length > 0) {
+    errorMessage(`${EXISTING_FILE_ERROR_MSG}:\n${existingFiles.join(', ')}`);
+  }
+  return existingFiles;
+}
+
+getExistingS3Files(bucket, dest, s3Client)
+.then(existing)
+.then(files => {
+  const uploads = files.map(file => s3Upload(bucket, file, src, dest, s3Client));
+  Promise.all(uploads)
+    .then(v => console.log)
+    .catch(e => console.error);
+})
+.catch(error => {
+  errorMessage(error.message);
 });
+
+//if (fs.lstatSync(src).isDirectory()) {
