e9178f3c7fdbd90ad003bceaf87ce8adf2f60e3b
Jesse Lamb <jesselamb@builddirect.com>
2016-12-16 15:46:56 -0800

    :star: adds latest version, uploads

---
+++ src/invalidate.js
@@ -0,0 +1,9 @@
+const DISTRIBUTION_ID_MAP = {
+  'bdcdn-dev': 'E2GX7EXNNOP81Q',
+  'bdcdn-ci' 'E2Y40Z4XPHSPWF',
+  'bdcdn-qa': 'E3IS939CHFZZ27',
+  'bdcdn-pe': 'E1ZRFU42QS5LW8',
+  'bdcdn-stage': 'E23AG6STLTGQI9'
+  'bdcdn-prod': 'EDH46CBWJMUTD'
+};
+
